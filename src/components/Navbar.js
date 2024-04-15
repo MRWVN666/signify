7203344079bc5915904d4a19cea7032f299f19b9
MRWVN666 <marwanammar55@gmail.com>
2024-04-15 16:41:32 +0200

    bahebak

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const img = "/signify/src/assets/logo.jpg"
+
+const Navbar = () => {
+  return (
+    <div className='Nav'>
+      <img src="../assets/logo.jpg" alt='logo' className='Logo' />
+      <h1 className='Text'>Signify</h1>
+    </div>
+  )
+}
+
+export default Navbar
